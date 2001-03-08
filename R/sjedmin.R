31f72f39beccd31e9f76a1a5634c2925e7143abf
Stephen J. Eglen <sje30@cam.ac.uk>
2001-03-08 11:44:58 +0000

    About to put in dminlul -- bounded version of Lucia's dmin rule.

--- R/sjedmin.R
+++ R/sjedmin.R
@@ -4,11 +4,11 @@
 
 dyn.load("/home/stephen/langs/R/c-code/pairwise_amac.so");
 
-dminmaxattempts <- 5                    #number of attempts before giving up.
+dminmaxattempts <- 20                    #number of attempts before giving up.
 
 dminsd <- function(wid = 1000, ht = 1000, npts = 200,
-                   dmin = 20, dminsd = 2) {
-
+                   dmin = 20, dminsd = 2)
+{
   attempt <- 1
   okay <- TRUE
   trying <- TRUE
@@ -31,7 +31,8 @@ dminsd <- function(wid = 1000, ht = 1000, npts = 200,
       attempt <- attempt + 1
   }
   if (attempt >= dminmaxattempts) {
-    warning("dminsd: couldnt do the dmin after many attempts")
+    cat(paste ("dminsd: ", dmin, dminsd, "fail after",
+                dminmaxattempts, "tries\n"))
     ## just make a random distribution instead of a nice mosaic.
     z$x <- (runif(npts) * wid)
     z$y <- (runif(npts) * ht)
@@ -41,16 +42,18 @@ dminsd <- function(wid = 1000, ht = 1000, npts = 200,
   ## Even if we couldn't make it, just return something?
   ## Make up the return list.
   note <- paste("dminsd", dmin, dminsd)
-  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay)
+  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
+       attempts = attempt, note = note)
   
 }
 
 
 dminl <- function(wid = 1000, ht = 1000, npts = 200,
-                   dmin = 20, dminsd = 2) {
-  ## Lucia's version of dmin -- new dmin value generated every step.
+                   dmin = 20, dminsd = 2)
+{
   attempt <- 1
-  trying <- TRUE;
+  okay <- TRUE
+  trying <- TRUE
   while (trying && (attempt < dminmaxattempts)) {
     z <- .C("dminl",
             as.double(wid),
@@ -69,12 +72,70 @@ dminl <- function(wid = 1000, ht = 1000, npts = 200,
     else
       attempt <- attempt + 1
   }
-  if (attempt >= dminmaxattempts)
-    warning("couldnt do the dmin after many attempts")
+  if (attempt >= dminmaxattempts) {
+    cat(paste ("dminsd: ", dmin, dminsd, "fail after",
+                dminmaxattempts, "tries\n"))
+    ## just make a random distribution instead of a nice mosaic.
+    z$x <- (runif(npts) * wid)
+    z$y <- (runif(npts) * ht)
+    okay <- FALSE
+  }
 
+  ## Even if we couldn't make it, just return something?
   ## Make up the return list.
   note <- paste("dminl", dmin, dminsd)
-  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, note=note)
+  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
+       attempts = attempt, note = note)
+  
 }
+
+
+damac <- function(wid = 1000, ht = 1000, npts = 200,
+                  d1 = 10, upper = 20, beta = 1.5)
+{
+  attempt <- 1
+  okay <- TRUE
+  trying <- TRUE
+  while (trying && (attempt < dminmaxattempts)) {
+    z <- .C("pairwise_amac",
+            as.double(wid),
+            as.double(ht),
+            as.integer(npts),
+            as.double(d1),
+            as.double(upper),
+            as.double(beta),
+            ## create memory to store return values.
+            x = double(npts),
+            y = double(npts),
+            )
+    trying <- FALSE
+  }
+
+  ## Even if we couldn't make it, just return something?
+  ## Make up the return list.
+  note <- paste("damac", d1, beta, upper)
+  list(x = z$x, y = z$y, note = note)
+  
+}
+
+hamac <- function(t, lower, upper, beta)
+{
+  ## Test the hamac function
+  ## ts <- seq(0, 100, by=1);
+  ## hs <- lapply(ts, function (x) hamac(x, 19, 76, 0.3)); plot(ts, hs)
+
+  z <- .C("h_amac",
+          as.double(t),
+          as.double(lower),
+          as.double(upper),
+          as.double(beta),
+          ## create memory to store return values.
+          h = double(1),
+          )
+  z$h
+}
+
+
+
 
 ## dyn.unload("/home/stephen/langs/R/c-code/pairwise_amac.so");
