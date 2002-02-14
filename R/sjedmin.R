ce14624512d43946ccdb2fe5b7e1faf19f830547
Stephen J. Eglen <sje30@cam.ac.uk>
2002-02-14 18:12:17 +0000

    dminlul: new function.

--- R/sjedmin.R
+++ R/sjedmin.R
@@ -2,7 +2,10 @@
 ##
 ## Load with source("/home/stephen/langs/R/c-code/dminsd.r")
 
-dyn.load("/home/stephen/langs/R/c-code/pairwise_amac.so");
+if (is.loaded(symbol.C("dminl")))
+  dyn.unload("~/langs/R/c-code/pairwise_amac.so")
+
+dyn.load("~/langs/R/c-code/pairwise_amac.so")
 
 dminmaxattempts <- 20                    #number of attempts before giving up.
 
@@ -73,7 +76,7 @@ dminl <- function(wid = 1000, ht = 1000, npts = 200,
       attempt <- attempt + 1
   }
   if (attempt >= dminmaxattempts) {
-    cat(paste ("dminsd: ", dmin, dminsd, "fail after",
+    cat(paste ("dminl: ", dmin, dminsd, "fail after",
                 dminmaxattempts, "tries\n"))
     ## just make a random distribution instead of a nice mosaic.
     z$x <- (runif(npts) * wid)
@@ -84,6 +87,191 @@ dminl <- function(wid = 1000, ht = 1000, npts = 200,
   ## Even if we couldn't make it, just return something?
   ## Make up the return list.
   note <- paste("dminl", dmin, dminsd)
+  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
+       attempts = attempt, note = note)
+  
+}
+
+
+dminlul <- function(wid = 1000, ht = 1000, npts = 200,
+                    dmin = 20, dminsd = 2, lower = 0, upper = 100)
+{
+  attempt <- 1
+  okay <- TRUE
+  trying <- TRUE
+  while (trying && (attempt < dminmaxattempts)) {
+    z <- .C("dminlul",
+            as.double(wid),
+            as.double(ht),
+            as.integer(npts),
+            as.double(dmin),
+            as.double(dminsd),
+            as.double(lower), as.double(upper),
+            ## create memory to store return values.
+            x = double(npts),
+            y = double(npts),
+            dmins = double(npts),
+            nrejects = integer(npts),
+            )
+    if (z$x[1] > 0)
+      trying <- FALSE
+    else
+      attempt <- attempt + 1
+  }
+  if (attempt >= dminmaxattempts) {
+    cat(paste ("dminlul: ", dmin, dminsd, "fail after",
+                dminmaxattempts, "tries\n"))
+    ## just make a random distribution instead of a nice mosaic.
+    z$x <- (runif(npts) * wid)
+    z$y <- (runif(npts) * ht)
+    okay <- FALSE
+  }
+
+  ## Even if we couldn't make it, just return something?
+  ## Make up the return list.
+  note <- paste("dminlul", dmin, dminsd, lower, upper)
+  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
+       attempts = attempt, note = note)
+  
+}
+
+dminlulbd <- function(wid = 1000, ht = 1000, npts = 200,
+                    dmin = 20, dminsd = 2, lower = 0, upper = 100)
+{
+  ## Birth and Death version of dminlul
+  attempt <- 1
+  okay <- TRUE
+  trying <- TRUE
+  while (trying && (attempt < dminmaxattempts)) {
+    z <- .C("dminlulbd",
+            as.double(wid),
+            as.double(ht),
+            as.integer(npts),
+            as.double(dmin),
+            as.double(dminsd),
+            as.double(lower), as.double(upper),
+            ## create memory to store return values.
+            x = double(npts),
+            y = double(npts),
+            dmins = double(npts),
+            nrejects = integer(npts),
+            )
+    if (z$x[1] > 0)
+      trying <- FALSE
+    else
+      attempt <- attempt + 1
+  }
+  if (attempt >= dminmaxattempts) {
+    cat(paste ("dminlulbd: ", dmin, dminsd, "fail after",
+                dminmaxattempts, "tries\n"))
+    ## just make a random distribution instead of a nice mosaic.
+    z$x <- (runif(npts) * wid)
+    z$y <- (runif(npts) * ht)
+    okay <- FALSE
+  }
+
+  ## Even if we couldn't make it, just return something?
+  ## Make up the return list.
+  note <- paste("dminlulbd", dmin, dminsd, lower, upper)
+  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
+       attempts = attempt, note = note)
+  
+}
+
+
+dminacc <- function(wid = 1000, ht = 1000, npts = 200,
+                    acc, dmax = 10, inc=1.0)
+{
+  ## Shapiro et al. method for using acceptance function created directly
+  ## from the K function of the exptl mosaic.  `acc' is the acceptance
+  ## function created by `make.acc' and dmax is the maximal distance over
+  ## which acceptance function is measured.  Cells with  NND greater than dmax
+  ## are automatically accepted.
+  attempt <- 1
+  okay <- TRUE
+  trying <- TRUE
+  while (trying && (attempt < dminmaxattempts)) {
+    z <- .C("dminacc",
+            as.double(wid),
+            as.double(ht),
+            as.integer(npts),
+            as.double(acc),
+            as.double(dmax),
+            as.double(inc),
+            ## create memory to store return values.
+            x = double(npts),
+            y = double(npts),
+            nrejects = integer(npts),
+            )
+    if (z$x[1] > 0)
+      trying <- FALSE
+    else
+      attempt <- attempt + 1
+  }
+  if (attempt >= dminmaxattempts) {
+    cat(paste ("dminsd: ", dmin, dminsd, "fail after",
+                dminmaxattempts, "tries\n"))
+    ## just make a random distribution instead of a nice mosaic.
+    z$x <- (runif(npts) * wid)
+    z$y <- (runif(npts) * ht)
+    okay <- FALSE
+  }
+
+  ## Even if we couldn't make it, just return something?
+  ## Make up the return list.
+  note <- paste("dmin.acc", dmax)
+  list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
+       attempts = attempt, note = note)
+  
+}
+
+make.acc <- function (l, dmax) {
+  ## Make an acceptance function from a L function.
+  ## This is useful for dmin.acc() and dminacc.bd()
+  k <- l^2 * pi; kmax <- k[dmax+1] #1 since steps starts from 0um.
+  acc <- k / kmax; acc <- pmin(acc, 1.0)
+  acc
+}
+
+
+dminacc.bd <- function(wid = 1000, ht = 1000, npts = 200,
+                    acc, dmax = 10, inc=1.0)
+{
+  ## Birth and death version of dminacc().
+  
+  attempt <- 1
+  okay <- TRUE
+  trying <- TRUE
+  while (trying && (attempt < dminmaxattempts)) {
+    z <- .C("dminacc_bd",
+            as.double(wid),
+            as.double(ht),
+            as.integer(npts),
+            as.double(acc),
+            as.double(dmax),
+            as.double(inc),
+            ## create memory to store return values.
+            x = double(npts),
+            y = double(npts),
+            nrejects = integer(npts),
+            )
+    if (z$x[1] > 0)
+      trying <- FALSE
+    else
+      attempt <- attempt + 1
+  }
+  if (attempt >= dminmaxattempts) {
+    cat(paste ("dminacc.bd: ", dmax, "fail after",
+                dminmaxattempts, "tries\n"))
+    ## just make a random distribution instead of a nice mosaic.
+    z$x <- (runif(npts) * wid)
+    z$y <- (runif(npts) * ht)
+    okay <- FALSE
+  }
+
+  ## Even if we couldn't make it, just return something?
+  ## Make up the return list.
+  note <- paste("dminacc.bd", dmax)
   list(x = z$x, y = z$y, dmins = z$dmins, nrejects = z$nrejects, okay = okay,
        attempts = attempt, note = note)
   
@@ -134,8 +322,6 @@ hamac <- function(t, lower, upper, beta)
           )
   z$h
 }
-
-
 
 
 ## dyn.unload("/home/stephen/langs/R/c-code/pairwise_amac.so");
