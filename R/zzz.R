1b1e5cc4e36acde3d465057a77c121925aa2fa38
Stephen J. Eglen <sje30@cam.ac.uk>
2007-07-15 12:15:31 +0000

    Initial revision

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.First.lib <- function(lib, pkg)
+  library.dynam("sjedmin", pkg, lib)
+
+.Last.lib <- function (libpath) {
+  ## Run when the package is being unloaded.  This allows us to test
+  ## packages within same session when dynlib is updated.
+  library.dynam.unload("sjedmin", libpath)
+}
+
